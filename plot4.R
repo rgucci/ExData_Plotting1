6100288ef4aff1f087d062e75caf642e6199a30f
Russell JVM Gutierrez <rgucci@gmail.com>
2015-04-10 17:31:46 +0800

    removed unnecessary conversion of factor to char because this was already done in read.table

--- plot4.R
+++ plot4.R
@@ -15,17 +15,32 @@ timeFormat <- "%d/%m/%Y %H:%M:%S"
 x <- as.POSIXct(strptime(paste(data$Date, data$Time, sep=" "), 
                          format=timeFormat))
 
-#create power data
-y <- as.numeric(data$Global_active_power)
+#create submetered data
+y1 <- as.numeric(as.numeric(data$Sub_metering_1))
+y2 <- as.numeric(as.numeric(data$Sub_metering_2))
+y3 <- as.numeric(as.numeric(data$Sub_metering_3))
 
 #open png device
-png("plot2.png", width=480, height=480)
+png("plot4.png", width=480, height=480)
 
 #create the plot
-plot(x, y, 
-     type="l", 
-     xlab="", 
-     ylab="Global Active Power (kilowatts)")
+colors <- c("black", "red", "blue")
+legends <- c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+
+plot(x, y1, 
+     type="l",
+     col=colors[[1]],
+     xlab="",
+     ylab="Energy Submetering")
+lines(x, y2, 
+      type="l", col=colors[[2]])
+lines(x, y3, 
+      type="l", col=colors[[3]])
+
+legend("topright", 
+       legend=legends, 
+       lwd=3, 
+       col=colors)
 
 #close png device
 dev.off()
